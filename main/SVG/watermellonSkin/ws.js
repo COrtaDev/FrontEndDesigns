8a2564b9c63541916e507d527d75493e8f3d784e
Conrad Orta <corta.dev@gmail.com>
2021-06-01 20:36:46 -0400

    Create ws.js

---
+++ main/SVG/watermellonSkin/ws.js
@@ -0,0 +1,3 @@
+/* Minified (422 bytes):
+<svg xmlns="http://www.w3.org/2000/svg"><pattern id="a" width="80" height="1" patternUnits="userSpaceOnUse"><circle r="99" fill="#3f812d"/><path d="M0 0h32v1H0z" fill="#96c96d"/></pattern><filter id="b"><feTurbulence type="fractalNoise" baseFrequency=".02" numOctaves="3"/><feDisplacementMap in="SourceGraphic" scale="48"/></filter><rect x="-10%" y="-10%" width="120%" height="120%" fill="url(#a)" filter="url(#b)"/></svg>
+*/
